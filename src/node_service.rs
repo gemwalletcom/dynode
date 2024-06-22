5993571ed0e1bc45042ae03831f7c07748c7a5cd
gemcoder21 <104884878+gemcoder21@users.noreply.github.com>
2024-06-22 01:23:54 +0100

    Add support for uri and content headers

--- src/node_service.rs
+++ src/node_service.rs
@@ -1,5 +1,6 @@
 use bytes::Bytes;
 use http_body_util::{BodyExt, Full};
+use hyper::header::{self, HeaderName, USER_AGENT};
 use hyper::service::Service;
 use hyper::HeaderMap;
 
@@ -25,13 +26,14 @@ impl Service<Request<IncomingBody>> for NodeService {
 
     fn call(&self, req: Request<IncomingBody>) -> Self::Future {
         let host = req.headers().get("host").unwrap().to_str().unwrap();
+        //let USER_AGENT =
+        //println!("host: {}, user-agent: {}", host, req.headers().get("key"));
 
         match self.domains.get(host) {
             Some(domain) => {
-                println!("test : {:?}", domain);
-
-                let url = domain.urls.first().unwrap().url.clone();
-                let uri = url.parse::<hyper::Uri>().unwrap();
+                let domain_url = domain.urls.first().unwrap().url.clone();
+                let uri = domain_url + req.uri().to_string().as_str();
+                let uri = uri.parse::<hyper::Uri>().unwrap();
 
                 async move { proxy_pass(req, uri).await }.boxed()
             }
@@ -51,25 +53,37 @@ async fn proxy_pass(
     uri: hyper::Uri,
 ) -> Result<Response<Full<Bytes>>, Box<dyn std::error::Error + Send + Sync>> {
     let client = Client::builder(hyper_util::rt::TokioExecutor::new()).build(HttpsConnector::new());
-    let method = original_request.method();
 
+    // request
     let original_headers = original_request.headers().clone();
-
-    //TODO: Persist only needed headers
-    let mut new_headers = HeaderMap::new();
-    if let Some(value) = original_headers.get("Content-Type") {
-        new_headers.append("Content-Type", value.clone());
-    }
-
     let mut request = Request::builder()
-        .method(method)
+        .method(original_request.method())
         .uri(uri)
         .body(original_request.into_body())
-        .expect("msg");
+        .expect("invalid request params");
 
-    *request.headers_mut() = new_headers;
+    *request.headers_mut() = persist_headers(&original_headers, &vec![header::CONTENT_TYPE]);
 
-    let resp = client.request(request).await?;
-    let body = resp.collect().await?.to_bytes();
-    Ok(Response::new(Full::from(body)))
+    // response
+    let response = client.request(request).await?;
+    let resp_headers = response.headers().clone();
+    let body = response.collect().await?.to_bytes();
+
+    let mut new_response = Response::new(Full::from(body));
+    *new_response.headers_mut() = persist_headers(&resp_headers, &vec![header::CONTENT_TYPE]);
+
+    Ok(new_response)
+}
+
+pub fn persist_headers(headers: &HeaderMap, list: &[HeaderName]) -> HeaderMap {
+    headers
+        .iter()
+        .filter_map(|(k, v)| {
+            if list.contains(&k) {
+                Some((k.clone(), v.clone()))
+            } else {
+                None
+            }
+        })
+        .collect()
 }
